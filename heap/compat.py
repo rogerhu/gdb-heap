5ed55a99d6ebb191e5a5c14d90862593db1d6bc7
David Malcolm <dmalcolm@redhat.com>
2010-07-29 16:45:38 -0400

    Use correct keyword argument to gdb.execute; make boolean visible

--- heap/compat.py
+++ heap/compat.py
@@ -4,25 +4,25 @@ deal with varying capabilities of the underlying gdb and its python bindings
 '''
 import gdb
 
-# gdb.execute's as_string keyworda argument was added between F13 and F14.
+# gdb.execute's to_string keyword argument was added between F13 and F14.
 # See https://bugzilla.redhat.com/show_bug.cgi?id=610241
 
-_has_gdb_execute_as_string = True
+has_gdb_execute_to_string = True
 try:
     # This will either capture the result, or fail before executing,
     # so in neither case should we get noise on stdout:
-    gdb.execute('help', as_string=True)
+    gdb.execute('help', to_string=True)
 except TypeError:
-    _has_gdb_execute_as_string = False
+    has_gdb_execute_to_string = False
 
 def execute(command):
-    '''Equivalent to gdb.execute(as_string=True), returning the output as
+    '''Equivalent to gdb.execute(to_string=True), returning the output as
     a string rather than logging it to stdout.
 
     On gdb versions lacking this capability, it uses redirection and temporary
     files to achieve the same result'''
-    if _has_gdb_execute_as_string:
-        return gdb.execute(command, as_string = True)
+    if has_gdb_execute_to_string:
+        return gdb.execute(command, to_string = True)
     else:
         import tempfile
         f = tempfile.NamedTemporaryFile('r', delete=True)
@@ -39,8 +39,8 @@ def execute(command):
         return result
 
 def dump():
-    print ('Does gdb.execute have an "as_string" keyword argument? : %s' 
-           % _has_gdb_execute_as_string)
+    print ('Does gdb.execute have an "to_string" keyword argument? : %s' 
+           % has_gdb_execute_to_string)
 
 
 
