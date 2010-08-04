08b5cf99c649d1e3c129b4350d67075f00fc5dd3
David Malcolm <dmalcolm@redhat.com>
2010-08-04 11:28:12 -0400

    Licensing fixes
    Add copy of the LGPLv2.1 (from http://www.gnu.org/licenses/lgpl-2.1.txt)
    Add LGPLv2.1 (or later) headers to all files except for heap/python.py
    
    Add copy of the Python license (from 2.6.5)

--- heap/sqlite.py
+++ heap/sqlite.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2010  David Hugh Malcolm
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 def categorize_sqlite3(addr, usage_set, visited):
     # "struct sqlite3" is defined in src/sqliteInt.h, which is an internal header
     ptr_type = caching_lookup_type('sqlite3').pointer()    
