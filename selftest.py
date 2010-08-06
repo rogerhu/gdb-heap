0523eeeb8b4a2c093fb0ee0b04a9687bc9eecf12
David Malcolm <dmalcolm@redhat.com>
2010-08-06 09:24:45 -0400

    Add parser for the table output, and start using it to make selftests more robust

--- selftest.py
+++ selftest.py
@@ -25,7 +25,7 @@ import subprocess
 import sys
 import unittest
 import random
-
+from collections import namedtuple
 from test.test_support import run_unittest, findfile
 
 if sys.maxint == 0x7fffffff:
@@ -133,6 +133,175 @@ class TestProgram(object):
         
         # Check that the binary exists:
         assert(os.path.exists(self.name))
+
+class ColumnNotFound(Exception):
+    def __init__(self, colname):
+        self.colname = colname
+
+class RowNotFound(Exception):
+    def __init__(self, criteria, table):
+        self.criteria = criteria
+        self.table = table
+    def __str__(self):
+        return 'RowNotFound(%s) in:%s' % (self.criteria, self.table)
+
+class Criteria(object):
+    '''A list of (colname, value) criteria for searching rows in a table'''
+    def __init__(self, table, kvs):
+        self.kvs = kvs
+        self._by_index = [(table.find_col(attrname), value)
+                          for attrname, value in kvs]
+
+    def __str__(self):
+        return 'Criteria(%s)' % ','.join('%r=%r' % (attrname, value)
+                                         for attrname, value in self.kvs)
+
+    def is_matched_by(self, row):
+        for colindex, value in self._by_index:
+            if row[colindex] != value:
+                return False
+        return True
+
+class ParsedTable(object):
+    '''Parses output from heap.Table, for use in writing selftests'''
+    @classmethod
+    def parse_lines(cls, data):
+        '''Parse the lines in the string, returning a list of ParsedTable
+        instances'''
+        result = []
+        lines = data.splitlines()
+        start = 0
+        while start < len(lines):
+            sep_line = cls._find_separator_line(lines[start:])
+            if sep_line:
+                sep_index, colmetrics = sep_line
+                t = ParsedTable(sep_index, colmetrics, lines[start:])
+                result.append(t)
+                start += t.sep_index + 1 + len(t.rows)
+            else:
+                break
+        return result
+
+    # Column metrics:
+    ColMetric = namedtuple('ColMetric', ('offset', 'width'))
+        
+    def __init__(self, sep_index, colmetrics, lines):
+        self.sep_index, self.colmetrics = sep_index, colmetrics
+
+        # Parse column headings:
+        header_index = self.sep_index - 1
+        self.colnames = self._split_cells(lines[header_index])
+
+        # Parse rows:
+        self.rows = []
+        for line in lines[self.sep_index + 1:]:
+            if line == '':
+                break
+            self.rows.append(self._split_cells(line))
+
+        self.rawdata = '\n'.join(lines[header_index:len(self.rows)+2])
+
+    def __str__(self):
+        return self.rawdata
+            
+    def get_cell(self, x, y):
+        return self.rows[y][x]
+
+    def find_col(self, colname):
+        # Find the index of the column with the given name
+        for x, col in enumerate(self.colnames):
+            if colname == col:
+                return x
+        raise ColumnNotFound(colname)
+
+    def find_row(self, kvs):
+        # Find the first row matching the criteria, or raise RowNotFound
+        criteria = Criteria(self, kvs)
+        for row in self.rows:
+            if criteria.is_matched_by(row):
+                return row
+        raise RowNotFound(criteria, self)
+
+    def find_cell(self, kvs, attr2name):
+        criteria = Criteria(self, kvs)
+        row = self.find_row(kvs)
+        return row[self.find_col(attr2name)]
+
+    def _split_cells(self, line):
+        row = []
+        for col in self.colmetrics:
+            cell = line[col.offset: col.offset+col.width].lstrip()
+            if cell == '':
+                cell = None
+            else:
+                # Remove ',' separators from numbers:
+                m = re.match('^([0-9,]+)$', cell) # [0-9]\,
+                if m:
+                    cell = int(cell.replace(',', ''))
+                    
+            row.append(cell)
+        return tuple(row)
+
+    @classmethod
+    def _find_separator_line(cls, lines):
+        # Look for the separator line
+        # Return (index, tuple of ColMetric)
+        for i, line in enumerate(lines):
+            if line.startswith('-'):
+                widths = [len(frag) for frag in line.split('  ')]
+                coldata = []
+                offset = 0
+                for width in widths:
+                    coldata.append(cls.ColMetric(offset=offset, width=width))
+                    offset += width + 2
+                return (i, tuple(coldata))
+            
+
+# Test data for table parsing (edited fragment of output during development):
+test_table = '''
+junk line
+
+       Domain        Kind                 Detail  Count  Allocated size
+-------------  ----------  ---------------------  -----  --------------
+       python         str                         3,891         234,936
+uncategorized                        98312 bytes      1          98,312
+uncategorized                         1544 bytes     43          66,392
+uncategorized                         6152 bytes     10          61,520
+       python       tuple                         1,421          54,168
+                                           TOTAL  9,377         857,592
+
+another junk line
+'''
+
+class ParserTests(unittest.TestCase):
+    def test_table1(self):
+        tables = ParsedTable.parse_lines(test_table)
+        self.assertEquals(len(tables), 1)
+        pt = tables[0]
+
+        # Verify column names:
+        self.assertEquals(pt.colnames, ('Domain', 'Kind', 'Detail', 'Count', 'Allocated size'))
+
+        # Verify (x,y) lookup, and type conversions:
+        self.assertEquals(pt.get_cell(0, 0), 'python')
+        self.assertEquals(pt.get_cell(1, 3), None)
+        self.assertEquals(pt.get_cell(4, 5), 857592)
+
+        # Verify searching by value:
+        self.assertEquals(pt.find_col('Count'), 3)
+        self.assertEquals(pt.find_row([('Allocated size', 54168),]),
+                          ('python', 'tuple', None, 1421, 54168))
+        self.assertEquals(pt.find_cell([('Kind', 'str'),], 'Count'), 3891)
+
+        # Error-checking:
+        self.assertRaises(ColumnNotFound,
+                          pt.find_col, 'Ensure that a non-existant column raises an error')
+        self.assertRaises(RowNotFound,
+                          pt.find_row, [('Count', -1)])
+
+    def test_multiple_tables(self):
+        tables = ParsedTable.parse_lines(test_table * 5)
+        self.assertEquals(len(tables), 5)
 
 class DebuggerTests(unittest.TestCase):
 
@@ -398,6 +567,10 @@ public:
         # FIXME
 
 
+    def assertHasRow(self, table, kvs):
+        table.find_row(kvs)
+        # ...which will raise a RowNotFound exception
+
     def test_python(self):
         out = self.command_test(['python', '-c', 'id(42)'],
                                 commands=['set breakpoint pending yes',
@@ -406,21 +579,26 @@ public:
                                           'heap sizes',
                                           'heap'],
                                 breakpoint='builtin_id')
-        
-        # Ensure that the code detected instances of various python types we expect to be present:
-        self.assert_('str' in out)
-        self.assert_('tuple' in out)
-        self.assert_('dict' in out)
-        self.assert_('code' in out)
-        self.assert_('function' in out)
-        self.assert_('list' in out)
-
-        # and of other types:
-        self.assert_('string data' in out)
-        self.assert_('pool_header overhead' in out)
 
         # Re-enable this for debugging:
         # print out
+
+        tables = ParsedTable.parse_lines(out)
+        heap_sizes_out = tables[0]
+        heap_out = tables[1]
+        
+        # Ensure that the code detected instances of various python types we expect to be present:
+        for kind in ('str', 'tuple', 'dict', 'code', 'function', 'list'):
+            self.assertHasRow(heap_out, 
+                              [('Kind', kind), ('Domain', 'python')])
+
+        # and of other types:
+        self.assertHasRow(heap_out,
+                          [('Kind', 'string data'),
+                           ('Domain', 'C')])
+        self.assertHasRow(heap_out,
+                          [('Kind', 'pool_header overhead'),
+                           ('Domain', 'pyarena')])
 
 if __name__ == "__main__":
     unittest.main()
